feffa4b9a9aa51b4efca1cf6ece4a922dea277a0
Natan Oliveira <natan_aru@hotmail.com>
2026-05-26 12:12:35 -0300

    IA mais inteligente

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function enviarMensagem()
     let entrada = document.getElementById("entrada");
 
     let texto = entrada.value;
+    texto = texto.toLowerCase();
 
     let mensagens = document.getElementById("mensagens");
 
@@ -12,21 +13,91 @@ function enviarMensagem()
 
     let resposta = "";
 
-    if(texto == "oi")
+    if(texto.includes("oi"))
     {
-        resposta = "Olá 😄";
-    }
+      let respostas = [
+        "Olá 😄",
+        "Oi oi",
+        "E aí 😎",
+        "Olá humano🤖",
+        "Olá Sr(a) Gay",
+        "Pronto para estudar hoje ?📚 "
+      ];
 
-    else if(texto == "tudo bem")
+    else if(texto.includes("tudo bem"))
     {
         resposta = "Estou funcionando perfeitamente 🚀";
     }
 
-    else if(texto == "qual seu nome")
+    else if(texto.includes ("qual seu nome"))
     {
         resposta = "Sou o Natan em forma de IA🤖";
     }
+else if(texto.includes("if"))
+{
+    resposta = `
+    O IF é usado para tomar decisões no programa 🚀
 
+    Exemplo:
+
+    if(x > 5)
+    {
+        printf("Maior");
+    }
+
+    Se a condição for verdadeira, o código executa.
+    `;
+}
+
+else if(texto.includes("float"))
+{
+    resposta = `
+    Float é usado para números com vírgula.
+
+    Exemplo:
+
+    float altura = 1.75;
+    `;
+}
+
+else if(texto.includes("for"))
+{
+    resposta = `
+    O FOR é usado para repetição.
+
+    Exemplo:
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d", i);
+    }
+    `;
+}
+
+else if(texto.includes("vetor"))
+{
+    resposta = `
+    Vetor guarda vários valores.
+
+    Exemplo:
+
+    int numeros[5];
+    `;
+}
+
+else if(texto.includes("questão"))
+{
+    resposta = `
+    Pergunta 😄
+
+    Qual tipo guarda números com vírgula?
+
+    A) int
+    B) char
+    C) float
+    D) void
+    `;
+}
     else
     {
         resposta = "Não entendi 😢";
@@ -37,4 +108,13 @@ function enviarMensagem()
     `;
 
     entrada.value = "";
+    document
+.getElementById("entrada")
+.addEventListener("keypress", function(event)
+{
+    if(event.key === "Enter")
+    {
+        enviarMensagem();
+    }
+});
 }
